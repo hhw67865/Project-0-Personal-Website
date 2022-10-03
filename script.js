f31b692a61b8a03ce89cadd82c80d305b16a99ea
Henry Wu <hhw67865@gmail.com>
2022-10-03 07:58:01 -0400

    added java script and added the source to html

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaSCript";
-document.querySelector("body").appendChild(h2);
+let slideIndex = 1;
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 0) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
